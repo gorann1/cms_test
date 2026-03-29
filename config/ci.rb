418fe49c63c41462ab50f8d2c0992628309347be
Goran <goran.novacic@gmail.com>
2026-03-29 19:28:51 +0000

    fix itr

--- config/ci.rb
+++ config/ci.rb
@@ -6,19 +6,15 @@ CI.run do
   step 'Setup', 'bin/setup --skip-server'
 
   step 'Style: Ruby', 'bin/rubocop'
-  step 'JavaScript: lint', 'npm run lint'
-  step 'JavaScript: format', 'npm run format'
-  step 'JavaScript: types check', 'npm run check'
 
   step 'Security: Gem audit', 'bin/bundler-audit'
-  step 'Security: NPM vulnerability audit', 'npm audit'
+  step 'Security: Importmap vulnerability audit', 'bin/importmap audit'
   step 'Security: Brakeman code analysis', 'bin/brakeman --quiet --no-pager --exit-on-warn --exit-on-error'
-
-  step 'Tests: Rails', 'bin/rspec'
+  step 'Tests: Rails', 'bin/rails test'
   step 'Tests: Seeds', 'env RAILS_ENV=test bin/rails db:seed:replant'
 
   # Optional: Run system tests
-  # step "Tests: System", "bin/rails test:system"
+  step 'Tests: System', 'bin/rails test:system'
 
   # Optional: set a green GitHub commit status to unblock PR merge.
   # Requires the `gh` CLI and `gh extension install basecamp/gh-signoff`.
